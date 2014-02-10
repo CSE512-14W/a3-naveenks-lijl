3bc95d92ec1b59f9d744fb2b8fe4672135a69275
Naveen <naveenks@xeon.(none)>
2014-02-10 11:19:24 -0800

    removing json

--- mapscript.js
+++ mapscript.js
@@ -97,33 +97,41 @@ function hour(value) {
 }
 
 /*
-    map._initPathRoot()    
+  map._initPathRoot()
 
-    var svg = d3.select("#map").select("svg"),
-      g = svg.append("g");
+  var svg = d3.select("#map").select("svg"), g = svg.append("g");
+  
+  var day1 = [
+    {
+      "coordinates": [51.975, -169.566],
+      "depth": 87.7,
+      "mag": 4.9
+    },
+    {
+      "coordinates": [40.358, 141.54],
+      "depth": 100.5,
+      "mag": 4.6
+    } 
+  ]
+ 
+  day1.forEach(function(d) {
+    d.LatLng = new L.LatLng(d.coordinates[0],d.coordinates[1])
+  })
 
-    d3.json("test.json", function(collection) {
+  var feature = g.selectAll("circle")
+    .data(day1)
+    .enter().append("circle")
+    .attr('fill','red')
+    .attr("r", function (d) { return d.mag*10})
+    .transition().delay(300).duration(1000).attr('r',1).remove();
 
-        collection.day1.forEach(function(d) {
-          d.LatLng = new L.LatLng(d.coordinates[0],d.coordinates[1])
-        })
-      
-        var feature = g.selectAll("circle")
-          .data(collection.day1)
-          .enter().append("circle")
-          .attr('fill','red')
-          .attr("r", function (d) { return d.mag*10})
-          .transition().delay(300).duration(1000).attr('r',1).remove();
-
-        function update() {
-          feature.attr("cx",function(d) { return map.latLngToLayerPoint(d.LatLng).x})
-          feature.attr("cy",function(d) { return map.latLngToLayerPoint(d.LatLng).y})
-          feature.attr("r",function(d) { return d.mag/2*Math.pow(2,map.getZoom())})
-        }
-        
-        map.on("viewreset", update);
-        update();
-    })
+  function update() {
+    feature.attr("cx",function(d) { return map.latLngToLayerPoint(d.LatLng).x})
+    feature.attr("cy",function(d) { return map.latLngToLayerPoint(d.LatLng).y})
+    feature.attr("r",function(d) { return d.mag/14*Math.pow(2,map.getZoom())})
+  }
+  map.on("viewreset", update);
+  update();
 */
 
 /* test.json
